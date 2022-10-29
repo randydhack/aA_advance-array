6f518f9d81a3286769f156a422af155a1c10cbe1
randy hac <randydhacks@gmail.com>
2022-10-29 09:57:28 -0700

    hi

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,7 +12,11 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here
+// Your code here
+
+// hi huey
+
+
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
