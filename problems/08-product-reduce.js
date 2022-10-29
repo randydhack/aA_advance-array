5773d851d7e4574db9e9b1e1bcb2b8da58e73246
randy hac <randydhacks@gmail.com>
2022-10-29 11:20:51 -0700

    08 done

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -17,6 +17,10 @@ console.log(productWithReduce([4, 3])); // 12
 
 let productWithReduce = function(nums) {
     // Your code here
+    return nums.reduce((prev, curr) => {
+      return prev * curr
+    }, 1);
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -25,4 +29,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
